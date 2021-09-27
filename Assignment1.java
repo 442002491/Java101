944589c301390c9f8c7768e93a3f984d840a9d05
NawafAlhrbii <91111150+NawafAlhrbii@users.noreply.github.com>
2021-09-27 20:53:28 +0300

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,28 @@
+// Nawaf alhrbi
+// 442002491
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner my0bj = new Scanner(System.in)
+		float num1;
+		
+		float num2;
+		system.out.println("Enter a num");
+		num1 =my0bj.nextFloat();
+		System.out.println("Enter a num")
+		num2 = my0bj.nextFloat();
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 % num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 > num2);
+		System.out.println(num1 >= num2);
+		System.out.println(num1 < num2);
+		System.out.println(num1 <= num2);
+		System.out.println(num1 != num2);
+		System.out.println(num1 == num2);
+	}
+}
