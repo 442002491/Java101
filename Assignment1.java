60d68378996bd3ed65d8bb3f2c21a2c5cafa7632
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-29 11:54:06 +0300

    صحح الاخطاء المذكورة

--- Assignment1.java
+++ Assignment1.java
@@ -1,15 +1,20 @@
+// your grade is 3.5/5
+//@ there is no import for scanner ????
 // Nawaf alhrbi
 // 442002491
 
+//
 public class Main
 {
 	public static void main(String[] args) {
+		//@check the error ;
 		Scanner my0bj = new Scanner(System.in)
 		float num1;
 		
 		float num2;
 		system.out.println("Enter a num");
 		num1 =my0bj.nextFloat();
+		//@check the error;
 		System.out.println("Enter a num")
 		num2 = my0bj.nextFloat();
 		
