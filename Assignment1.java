1fbc8aa9e3bb05bf7d025beddf76d92606078b89
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-29 23:19:04 +0300

    there is two errors check your errors, import statement should be always at the first line

--- Assignment1.java
+++ Assignment1.java
@@ -1,21 +1,20 @@
-// your grade is 3.5/5
+// your grade is 3/5
 //@ there is no import for scanner ????
 // Nawaf alhrbi
 // 442002491
 
-//
- import java.util.Scanner;
+//@ set the import first line ( line 9 should be in line 1 )
+import java.util.Scanner;
+//@ change Main to Assignment1 ( Golden rule class should match the file name )
 public class Main
 {
 	public static void main(String[] args) {
-		//@check the error ;
 		Scanner my0bj = new Scanner(System.in);
 		float num1;
 		
 		float num2;
 		System.out.println("Enter a num");
 		num1 =my0bj.nextFloat();
-		//@check the error;
 		System.out.println("Enter a num");
 		num2 = my0bj.nextFloat();
 		
