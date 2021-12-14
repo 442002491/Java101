ea59744aad8ef6896b04d144a432e8fa4fc178dd
442002491 <91111150+442002491@users.noreply.github.com>
2021-12-14 21:39:08 +0300

    Create class Even_switch.java

---
+++ class Even_switch.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Even_switch.java
+        {
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number 1");
+
+        int g= scan.nextInt();
+        switch (g){
+        case 1:
+
+        int xn;
+        System.out.print("Enter a Number");
+        xn = scan.nextInt();
+        xn %=2;
+        if (n==0){
+        System.out.print("even");
+        }
+        else {
+        System.out.print("odd");
+        }
+
+
+        }}}
