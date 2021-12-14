48c6d3eb806acd131b478327538e7c53b66a68e0
442002491 <91111150+442002491@users.noreply.github.com>
2021-12-14 22:08:23 +0300

    Create PositiveAndAegative.java

---
+++ PositiveAndAegative.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PositiveAndAegative.java {
+    public static void main(String[] args) {
+        Scanner sacn = new Scanner(System.in);
+        float nono;
+        System.out.println("Enter the number");
+        nono = sacn.nextFloat();
+        if(num < 0) {
+            System.out.println("the number negative");
+        }
+        else {
+            System.out.println("the number postive");
+        }
+    }
+}
